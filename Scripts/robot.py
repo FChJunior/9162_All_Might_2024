db93447a76320fa0379de7eb58638404bba1b610
Chagas <jniors75@gmail.com>
2024-01-17 10:08:58 -0300

    Criação da Classe Principal SMASH
    Esse commit é referente a criação da classe principal com todas as funcionalidades necessárias.

--- Scripts/robot.py
+++ Scripts/robot.py
@@ -1,17 +1,44 @@
-#---------------- Smash FRC 2024 - CRECENDO -----------------#
+#----------------- Smash FRC 2024 - CRECENDO ------------------#
 # Equipe: 9162 All Migth
 # Robô: Smash
 # Programadores: Niord e João Antônio
 # Mentor: Chagas Junior
+# 
+# ----------------------- Descrição: ------------------------- #
+# Essa é a classe principal onde chamará todas as ouutras classe
+# e criar os objetos de funcionalidades de cada subsitema que 
+# trabalharemos no SMASH para a temporada 2024 CRECENDO.
+# 
 
-import wpilib 
+
+# ------------------- Bibliotecas e Classes ------------------- #
+# --> wpilib: Biblioteca principal da competição. Nele encontraremos
+# todos os metódos e classes que serão fundamental para o funcionamento
+# do robô.
+#
+
+import wpilib as wp
 
 
-class Smash (wpilib.TimedRobot):
-
+class Smash (wp.TimedRobot):
     def robotInit(self) -> None:
-        self.joy = wpilib.XboxController(0)
+        pass
+    
+    def robotPeriodic(self) -> None:
+        pass
+
+    def autonomousInit(self) -> None:
+        pass
+
+    def autonomousPeriodic(self) -> None:
+        pass
+
+    def teleopInit(self) -> None:
+        pass
 
     def teleopPeriodic(self) -> None:
-        if self.joy.getAButton():
-            self.logger.info("pressionado")
+        pass
+
+
+if Smash == "_main_":
+    wp.run(Smash)
