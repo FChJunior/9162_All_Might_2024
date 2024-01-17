d9ba2b58a68a6f1cadac1312d77a1fa36ab3c308
Chagas <jniors75@gmail.com>
2024-01-17 15:48:21 -0300

    Método Drive Train
    Método de controle do drive train.

--- Scripts/robot.py
+++ Scripts/robot.py
@@ -1,29 +1,58 @@
 #----------------- Smash FRC 2024 - CRECENDO ------------------#
 # Equipe: 9162 All Migth
-# Robô: Smash
+# Robô: Smash 3.0
 # Programadores: Niord e João Antônio
 # Mentor: Chagas Junior
+#--------------------------------------------------------------#
+
+# ----------------------- Descrição: --------------------------#
 # 
-# ----------------------- Descrição: ------------------------- #
-# Essa é a classe principal onde chamará todas as ouutras classe
-# e criar os objetos de funcionalidades de cada subsitema que 
-# trabalharemos no SMASH para a temporada 2024 CRECENDO.
-# 
+# -------------------------------------------------------------#
 
 
-# ------------------- Bibliotecas e Classes ------------------- #
-# --> wpilib: Biblioteca principal da competição. Nele encontraremos
-# todos os metódos e classes que serão fundamental para o funcionamento
-# do robô.
-#
+# ------------------------ Bibliotecas -------------------------#
+# 
+#---------------------------------------------------------------#
 
 import wpilib as wp
+import wpilib.drive as drive
+import rev
+import phoenix6.hardware as ph
 
+
+#------------------- Classe de Funcionamento -------------------#
+# 
+#---------------------------------------------------------------#
 
 class Smash (wp.TimedRobot):
+
+# ------------------ Metodo Inicial do Robô --------------------#
     def robotInit(self) -> None:
-        pass
-    
+        self.joystick    = wp.Joystick(0)
+        self.control     = wp.XboxController(1)
+
+        self.motorLeft1  = rev.CANSparkMax(1,rev.CANSparkMax.MotorType.kBrushed)
+        self.motorLeft2  = rev.CANSparkMax(2,rev.CANSparkMax.MotorType.kBrushed)
+        self.motorRight1 = rev.CANSparkMax(3,rev.CANSparkMax.MotorType.kBrushed)
+        self.motorRight2 = rev.CANSparkMax(4,rev.CANSparkMax.MotorType.kBrushed)
+
+        self.motorLeft   = wp.MotorControllerGroup(self.motorLeft1, self.motorLeft2)
+        self.motorRight  = wp.MotorControllerGroup(self.motorRight1, self.motorRight2)
+
+        self.driveTrain = drive.DifferentialDrive(self.motorLeft, self.motorRight)
+# --------------------------------------------------------------#
+        
+
+    def driveTrainController(self):
+        in_control = self.joystick.getRawAxis(1)
+
+        speed = (((in_control - 1) * (1)) / -2) + 0
+        xSpeed = self.joystick.getRawAxis(2) * speed
+        zSpeed = self.joystick.getRawAxis(1) * speed
+
+        self.driveTrain.arcadeDrive(xSpeed, zSpeed, False)
+
+
     def robotPeriodic(self) -> None:
         pass
 
@@ -37,8 +66,8 @@ class Smash (wp.TimedRobot):
         pass
 
     def teleopPeriodic(self) -> None:
-        pass
+        self.driveTrainController()
 
-
-if Smash == "_main_":
+    
+if __name__ == "__main__":
     wp.run(Smash)
